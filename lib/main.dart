30cd54f20d301fcffff569870417265176ea3bcc
oishigyunyu <yamamuuuura@gmail.com>
2022-06-07 20:30:09 +0900

    exerciseの途中まで

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: unused_element
+
+// You can read about packages here: https://flutter.dev/using-packages/
 import 'package:flutter/material.dart';
 
+// You can use a relative import, i.e. `import 'category.dart';` or
+// a package import, as shown below.
+// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
+import './category.dart';
+
+// TODO: Pass this information into your custom [Category] widget
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+/// The function that is called when main.dart is run.
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const UnitConverterApp());
+}
+
+/// This widget is the root of our application.
+/// Currently, we just show one widget in our app.
+class UnitConverterApp extends StatelessWidget {
+  const UnitConverterApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Rectangle"),
-        ),
-        body: Container(
-          color: Colors.greenAccent,
+        backgroundColor: Colors.green[100],
+        body: const Center(
+          // TODO: Determine what properties you'll need to pass into the widget
+          child: Category(),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
+
